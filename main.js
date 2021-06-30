64d90efc15f3ceaa85c04de1f8b18a00f12199ca
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-06-30 18:17:49 +0100

    put everything inside a module fn

--- main.js
+++ main.js
@@ -1,43 +1,45 @@
 //SECTION navigation
-const startPage = document.getElementById("start-page");
-const singleplayerPage = document.getElementById("singleplayer-page");
-const multiplayerPage = document.getElementById("multiplayer-page");
-const gamePage = document.getElementById("game-page");
-// TODO work on header
+(() => {
+  const startPage = document.getElementById("start-page");
+  const singleplayerPage = document.getElementById("singleplayer-page");
+  const multiplayerPage = document.getElementById("multiplayer-page");
+  const gamePage = document.getElementById("game-page");
+  // TODO work on header
 
-//* Start Page
+  //* Start Page
 
-const singleplayerButton = document.getElementById("singleplayer-button");
-const multiplayerButton = document.getElementById("multiplayer-button");
+  const singleplayerButton = document.getElementById("singleplayer-button");
+  const multiplayerButton = document.getElementById("multiplayer-button");
 
-singleplayerButton.addEventListener("click", () => {
-  startPage.classList.add("hidden");
-  singleplayerPage.classList.remove("hidden");
-});
-
-multiplayerButton.addEventListener("click", () => {
-  startPage.classList.add("hidden");
-  multiplayerPage.classList.remove("hidden");
-});
-
-//* SingleplayerPage MultiplayerPage GamePage
-
-const backButtons = document.querySelectorAll(".backbutton");
-const startButtons = document.querySelectorAll(".startbutton");
-
-backButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    startPage.classList.remove("hidden");
-    singleplayerPage.classList.add("hidden");
-    multiplayerPage.classList.add("hidden");
-    gamePage.classList.add("hidden");
+  singleplayerButton.addEventListener("click", () => {
+    startPage.classList.add("hidden");
+    singleplayerPage.classList.remove("hidden");
   });
-});
 
-startButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    singleplayerPage.classList.add("hidden");
-    multiplayerPage.classList.add("hidden");
-    gamePage.classList.remove("hidden");
+  multiplayerButton.addEventListener("click", () => {
+    startPage.classList.add("hidden");
+    multiplayerPage.classList.remove("hidden");
   });
-});
+
+  //* SingleplayerPage MultiplayerPage GamePage
+
+  const backButtons = document.querySelectorAll(".backbutton");
+  const startButtons = document.querySelectorAll(".startbutton");
+
+  backButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      startPage.classList.remove("hidden");
+      singleplayerPage.classList.add("hidden");
+      multiplayerPage.classList.add("hidden");
+      gamePage.classList.add("hidden");
+    });
+  });
+
+  startButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      singleplayerPage.classList.add("hidden");
+      multiplayerPage.classList.add("hidden");
+      gamePage.classList.remove("hidden");
+    });
+  });
+})();
